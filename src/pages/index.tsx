162de44729e7ad97fe0fbc48329ae4004c9e6afd
Mamadou Ly <mamadou.ly@optisantis.com>
2021-04-25 13:58:11 +0200

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Box } from '@chakra-ui/react';
 import { Layout } from 'components/Layout/Layout';
 import { ProductsList } from 'components/products/ProductsList';
-import { useProducts } from 'lib/api/products/useProducts';
 import React, { ReactElement } from 'react';
 
 export default function HomePage(): ReactElement {
-    const { data } = useProducts();
-    console.log(data);
-    return <Layout>{data && <ProductsList products={data.products} />}</Layout>;
+    return (
+        <Layout>
+            <ProductsList />
+        </Layout>
+    );
 }
